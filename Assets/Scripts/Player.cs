23d23402aeafca33eb2ea028eb4c2f727be6927f
Salzspieler <long.heiligentag@gmx.de>
2025-04-23 14:22:04 +0200

    Level von Leveldesigner hinzugefuegt
    Level von Leveldesigner hinzugefuegt

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public float speed = 5f;
+    public bool isGrounded = false;
+
+    public float jumpHeight = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,52 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Movement
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+
+        //jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+            //rb.velocity = Vector2.up * speed;
+        }
+
+        //spin
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
+
+
+    //groundCheck
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //check anderes Objekt ob es Ground ist
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            //animator.SetBool("isGrounded", true);
+        }
+
+
+    }
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+
+
+
+        //animator.SetBool("isGrounded", false);
+    }
+
+
 }
